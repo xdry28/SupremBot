d8826f3ae5d32f212340fef9d3b45cb99aad3b5f
xdry28 <43451993+xdry28@users.noreply.github.com>
2018-12-17 18:27:07 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client
+var prefix = ("$")
+
+bot.on('ready', function () {
+    bot.user.setActivity("Détruire l'humanité, $help")
+    console.log("Bot connecté avec succès")
+})
+bot.login(process.env.TOKEN)
+
+bot.on('message', function (message) {
+    if (message.content === '$work') {
+        message.reply("Seul l'Empereur peut m'utiliser comme il le souhaite")
+    }
+    if (message.content === prefix + "help"){
+        message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
+        console.log("Commande help demandé")
+    }
+    if (message.author === 'xdry28') {
+        message.channel.send("**SupremBot**")
+    }
+})
