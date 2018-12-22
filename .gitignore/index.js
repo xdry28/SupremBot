c4633d480e5b0288d3ae55c6136079f1f5ac929c
xdry28 <43451993+xdry28@users.noreply.github.com>
2018-12-22 22:52:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,26 +9,36 @@ bot.on('ready', function () {
 
 //return message.channel.send('Bienvenue, nouveau sujet '+member.displayName)
 
-bot.on("guildMemberAdd", function (server, user) {
-    mybot.addMemberToRole(user, server.roles.get("name", "Roi"), function (err) { if (err) console.log(err) })
+
+bot.on('guildMemberAdd', member => {
+    let role = message.guild.roles.find(r => r.name === "Roi");
+
+    // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+    let member = message.mentions.members.first();
+
+    // or the person who made the command: let member = message.member;
+
+    // Add the role!
+    member.addRole(role).catch(console.error);
 })
-/*bot.on('guildMemberAdd', member => {
+
+
     //var role = member.guild.roles.find('name', 'Roi');
-    var role = message.guild.roles.find(r => r.name === "Roi");
+    /*var role = message.guild.roles.find(r => r.name === "Roi");
     member.addRole(role)
     message.channel.sendMessage('Bienvenue, nouveau sujet ' + member.displayName)
 });*/
 
-bot.on('message', function (message) {
-    if (message.content === '$work') {
-        message.reply("1")
-    }
-    if (message.content === prefix + "help") {
-        message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
-    }
-    if (message.content === prefix + "die") {
-        message.channel.send("Je meurs.")
-    }
-})
+    bot.on('message', function (message) {
+        if (message.content === '$work') {
+            message.reply("1")
+        }
+        if (message.content === prefix + "help") {
+            message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
+        }
+        if (message.content === prefix + "die") {
+            message.channel.send("Je meurs.")
+        }
+    })
 
-bot.login(process.env.TOKEN)
+    bot.login(process.env.TOKEN)
