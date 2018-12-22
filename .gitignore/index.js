896a2fc299e0dc613f3292ad3ab831625f09be35
xdry28 <43451993+xdry28@users.noreply.github.com>
2018-12-22 22:37:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,18 +10,20 @@ bot.on('ready', function () {
 //return message.channel.send('Bienvenue, nouveau sujet '+member.displayName)
 
 bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', 'Roi');
+    //var role = member.guild.roles.find('name', 'Roi');
+    var role = message.guild.roles.find(r => r.name === "Roi");
     member.addRole(role)
+    message.channel.sendMessage('Bienvenue, nouveau sujet ' + member.displayName)
 });
 
 bot.on('message', function (message) {
     if (message.content === '$work') {
         message.reply("1")
     }
-    if (message.content === prefix + "help"){
+    if (message.content === prefix + "help") {
         message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
     }
-    if (message.content === prefix + "die"){
+    if (message.content === prefix + "die") {
         message.channel.send("Je meurs.")
     }
 });
