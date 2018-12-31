9bf845719788566f0c5bb315d30e120dbae855ce
xdry28 <43451993+xdry28@users.noreply.github.com>
2018-12-31 14:20:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,26 +5,20 @@ var prefix = ("$")
 bot.on('ready', function () {
     bot.user.setActivity("Détruire l'humanité")
     console.log("Bot connecté")
+    
 })
 
 //return message.channel.send('Bienvenue, nouveau sujet '+member.displayName)
 
-bot.on('guildMemberAdd', member => {
-    let role = message.guild.roles.find(r => r.name === "Roi");
 
-    // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
-    let member = message.mentions.members.first();
-
-    // or the person who made the command: let member = message.member;
-
-    // Add the role!
-    member.addRole(role).catch(console.error);
-})
-    //var role = member.guild.roles.find('name', 'Roi');
-    /*var role = message.guild.roles.find(r => r.name === "Roi");
-    member.addRole(role)
+bot.on('guildMemberAdd', function (member) {
+  
+    message.member.addRole('526144681361539097')
+    .then(console.log)
+    .catch(console.error);
     message.channel.sendMessage('Bienvenue, nouveau sujet ' + member.displayName)
-});*/
+})
+
 
     bot.on('message', function (message) {
         if (message.content === '$work') {
