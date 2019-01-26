fb92b3802e2f846978029836b7c2e57feb0efc5c
xdry28 <43451993+xdry28@users.noreply.github.com>
2019-01-26 11:12:29 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,33 +3,33 @@ const bot = new Discord.Client
 var prefix = ("$")
 
 bot.on('ready', function () {
-    bot.user.setActivity("Détruire l'humanité")
-    console.log("Bot connecté")
-    
+    bot.user.setActivity("Détruire l'humanité").catch(console.error)
+    console.log("Bot connecté").catch(console.error)
+
 })
 
 //return message.channel.send('Bienvenue, nouveau sujet '+member.displayName)
 
 
 bot.on('guildMemberAdd', function (member) {
-  
-    message.member.addRole('Roi')
-    .then(console.log)
-    .catch(console.error);
+
+    message.member.addRole('522902109902077973')
+        .then(console.log)
+        .catch(console.error);
     message.channel.send('Bienvenue, nouveau sujet ' + member.displayName)
 })
 
 
-    bot.on('message', function (message) {
-        if (message.content === '$work') {
-            message.reply("1")
-        }
-        if (message.content === prefix + "help") {
-            message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
-        }
-        if (message.content === prefix + "die") {
-            message.channel.send("Je meurs.")
-        }
-    })
+bot.on('message', function (message) {
+    if (message.content === '$work') {
+        message.reply("1")
+    }
+    if (message.content === prefix + "help") {
+        message.channel.send("**SupremBot** utilise le préfixe **$** pour fonctionner et permet entre autre d'aider l'Empereur Suprême dans sa gestion du serveur")
+    }
+    if (message.content === prefix + "die") {
+        message.channel.send("Je meurs.")
+    }
+})
 
-    bot.login(process.env.TOKEN)
+bot.login(process.env.TOKEN)
